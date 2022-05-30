89c39dc197839e763af0e802040fc19fb0d7d7c0
minarypenguin <alexfinhart@gmail.com>
2022-05-30 18:02:49 +0800

    addons: themes: Add removed iconpacks as WiFi and Signal overlays
    Signed-off-by: minarypenguin <alexfinhart@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>

--- config.mk
+++ config.mk
@@ -194,12 +194,17 @@ PRODUCT_PACKAGES += \
 
 # Signal / WiFi Icons
 PRODUCT_PACKAGES += \
+	AcherusWiFiOverlay \
+	AcherusSignalOverlay \
 	BarsSignalOverlay \
 	BarsWiFiOverlay \
 	DoraSignalOverlay \
 	DoraWiFiOverlay \
 	InsideSignalOverlay \
 	InsideWiFiOverlay \
+	OutlineWiFiOverlay \
+	OutlineSignalOverlay \
+	PUISignalOverlay \
 	RoundSignalOverlay \
 	RoundWiFiOverlay \
 	SneakySignalOverlay \
