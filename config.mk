78d3e069706247a802937b083bcf443dad8d6e3f
Trishiraj <gamerprince.exp@gmail.com>
2022-09-20 09:40:00 +0800

    QS panel Styles [3/3]
    Signed-off-by: str-br <alexfinhart@gmail.com>

--- config.mk
+++ config.mk
@@ -227,6 +227,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OmniJaws
 
+# QS Panel
+PRODUCT_PACKAGES += \
+    QSOutline \
+    QSTwoToneAccent
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
