e2e44e7ab1e4e249b0bb6cdaf37d44a4dd960829
minaripenguin <minaripenguin@users.noreply.github.com>
2022-12-28 23:03:31 +0700

    extras: UIStyle: Add Vivid monet shades
    [travarilo]:
    - Adapt to our UIStyle configuration
    - Android.mk to Android.bp
    
    Change-Id: If02f0cb90282804be6cb99d7da30141ebc976aa8

--- config.mk
+++ config.mk
@@ -237,6 +237,7 @@ PRODUCT_PACKAGES += \
 
 # UI styles
 PRODUCT_PACKAGES += \
+    AndroidVividThemeOverlay \
     NexodusThemeAndroidOverlay \
     NexodusThemeSettingsOverlay \
     NexodusThemeSystemUIOverlay \
